4a04ec49e722c9ce617cb1b9e24bb8ccb8e10292
Patrick NDICUNGUYE <ndipazzo43@gmail.com>
2025-12-12 11:38:09 +0200

    Update schema.py

--- schema.py
+++ schema.py
@@ -1,33 +1,19 @@
 django-admin startproject alx_backend_graphql_crm
 cd alx_backend_graphql_crm
-
 python manage.py startapp crm
 
 pip install graphene-django django-filter
 
-alx_backend_graphql_crm/settings.py
-
 INSTALLED_APPS = [
-    "django.contrib.admin",
-    "django.contrib.auth",
-    "django.contrib.contenttypes",
-    "django.contrib.sessions",
-    "django.contrib.messages",
-    "django.contrib.staticfiles",
-
-    # Third-party
-    "graphene_django",
-
-    # Local app
+    ...
     "crm",
+    "graphene_django",
+    "django_filters",
 ]
 
 GRAPHENE = {
     "SCHEMA": "alx_backend_graphql_crm.schema.schema"
 }
-
-
-alx_backend_graphql_crm/schema.py
 
 import graphene
 
@@ -36,36 +22,25 @@ class Query(graphene.ObjectType):
 
 schema = graphene.Schema(query=Query)
 
-alx_backend_graphql_crm/urls.py
-
 from django.contrib import admin
 from django.urls import path
 from graphene_django.views import GraphQLView
 from django.views.decorators.csrf import csrf_exempt
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
+    path("admin/", admin.site.urls),
     path("graphql", csrf_exempt(GraphQLView.as_view(graphiql=True))),
 ]
 
 python manage.py runserver
 
-Open:
-
-http://localhost:8000/graphql
-
-Run this in the GraphiQL editor:
-
 {
   hello
 }
-
-Expected output:
 
 {
   "data": {
     "hello": "Hello, GraphQL!"
   }
 }
-
 
